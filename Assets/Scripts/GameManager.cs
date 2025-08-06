e52b8fed054826fce11287198c34f50d42408bb8
vahkd33114 <vahkd33114@yahoo.co.jp>
2025-08-06 22:34:00 +0900

    [add] FirstCommic

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
