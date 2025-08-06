124abf6301158a45ddb9a6e537e7723e91906b0e
vahkd33114 <vahkd33114@yahoo.co.jp>
2025-08-07 01:13:33 +0900

    [add] プレイヤーの挙動

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,41 @@
 using UnityEngine;
-using System;
-using UnityEngine.UI;
 
-
-public class NewMonoBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    
+    Rigidbody rb; 
+    [SerializeField] private float speed = 3.0f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        // Wキーで前進
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.linearVelocity = transform.forward * speed;
+        }
+        // Aキーで左方向
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.linearVelocity = - transform.right * speed;
+        }
+        // Sキーで後退
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.linearVelocity = - transform.forward * speed;
+        }
+        // Dキーで右方向
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.linearVelocity =  transform.right * speed;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
+    [SerializeField] private float speed = 3.0f;
+    
     void Update()
     {
-        
+        Move();
+    }
+    
+    
+    private void Move()
+    {
+        // Wキーで前進
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        }
+        // Aキーで左方向
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * (speed * Time.deltaTime);
+        }
+        // Sキーで後退
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * (speed * Time.deltaTime);
+        }
+        // Dキーで右方向
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * (speed * Time.deltaTime);
+        }
     }
 }
